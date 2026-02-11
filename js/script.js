1553f2e59d95f0db813c5bcc2a8cc45f8458113b
bekzod45 <abduazimovbekzod78@gmail.com>
2026-02-11 15:18:24 +0500

    nimadir

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+console.log("hello world");
+console.log("Mening kodim");
+console.log("GitHub dagi kod");
+
+function myFn() {
+  console.log("salom");
+}
+
+function myFn() {
+  console.log("salom");
+}
